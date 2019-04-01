7fa8a25a6d9b5e9478756a9ef8c3f65288ac499b
szymon199815 <szykacz@gmail.com>
2019-04-01 12:42:22 +0200

    podstawy

---
+++ 1_podstawy.py
@@ -0,0 +1,23 @@
+print("CDV")
+print(2)
+
+'''
+komentarz
+blokowy
+'''
+
+#potęgowanie
+potęga =2**10
+print(potęga)
+
+#pobieranie danych z klawiatury
+
+imie=input()
+#kontatenacja+
+print("Twoje imie podane z klawiatury: " + imie)
+
+nazwisko = input("Podaj nazwisko: " +)
+print("Twoje nazwisko podane z klawiatury: " + nazwisko)
+
+
+
